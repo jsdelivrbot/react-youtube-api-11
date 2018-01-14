d5cd4f8911f1e173665556452d805952a3ae5924
Chika Kawara <shikinami@gmail.com>
2018-01-14 18:34:56 +0900

    implement to show videodetail

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-// import VideoDetail from './components/video_detail';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCnzZHAVDT0jOswNuZ0ChIb1SD7CpvdDOs';
 
@@ -24,7 +24,7 @@ class App extends Component {
     return (
         <div>
           <SearchBar />
-          
+          <VideoDetail video={ this.state.videos[0] } />
           <VideoList videos={ this.state.videos } />
         </div>
       );
