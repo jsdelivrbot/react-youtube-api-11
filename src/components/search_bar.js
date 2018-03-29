aaa37c66c72ba2bd618c21d448f69ee11149784c
Chika Kawara <shikinami@gmail.com>
2018-03-29 10:18:33 +0900

    commit all of modified files

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,12 +8,22 @@ class SearchBar extends Component {
 
     this.state = { term: '' };
   }
+
   render() {
     return (
-      <div>
-      <input onChange={ event => this.setState({ term: event.target.value }) } />
+      <div className="search-bar">
+      <input
+        value={ this.state.term }
+        onChange={ event => this.onInputChange(event.target.value) }
+        // onChange={ event => this.setState({ term: event.target.value }) }
+       />
       </div>
     );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 // const SearchBar = () => {
--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,8 +3,14 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
   // const videos = props.videos;
+  // const selectedVideo = props.onVideoSelect;
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem key={ video.etag } video={ video } />
+    return (
+      <VideoListItem
+        onVideoSelect={ props.onVideoSelect }
+        key={ video.etag }
+        video={ video } />
+    );
   });
 
   return (
